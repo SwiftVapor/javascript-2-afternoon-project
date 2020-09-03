ce36c145db96b54664295aacec37f28d8ae18150
SwiftVapor <jonathongthayne@gmail.com>
2020-09-03 15:48:19 -0600

    Finishe array problems

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return (arr[0]);
+};
 
 
 ////////// PROBLEM 2 //////////
@@ -34,7 +36,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr){
+  return (arr[2]);
+};
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for (let i = 0; i < family.length; i++){
+    alert (family[i]);
+  }
+};
 
 ////////// PROBLEM 4 //////////
 
@@ -64,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters){
+  for (let i = letters.length - 1; i >= 0; i--){
+    alert (letters[i]);
+  }
+};
 
 ////////// PROBLEM 5 //////////
 
@@ -80,7 +92,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 
-
+function evenFinder(arr){
+  for (i = 0; i < arr.length ; i++) {
+      if (arr[i] % 2 === 1) {
+        arr.splice(i,1)
+      }
+  }
+      return arr;
+};
 
 
 
