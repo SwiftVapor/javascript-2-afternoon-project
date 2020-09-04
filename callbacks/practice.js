05a8af06787d86b799250c877113206ba7bbbafb
SwiftVapor <jonathongthayne@gmail.com>
2020-09-03 20:40:12 -0600

    finished Callbacks

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(array, callback){
+  return callback (array[0])
+};
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,11 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last (array, callback){
+  return callback(array[array.length-1])
+};
+
+
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,7 +74,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, cb){
+  return cb(num1*num2)
+};
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,6 +95,14 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains( array, name, cb){
+  if (array.includes(name) === true){
+    return cb(true);
+  }
+  else{
+    return cb(false);
+  }
+};
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,7 +124,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(array, cb){
+  let unique = [];
+  for (let i = 0; i < array.length; i++){
+    if (unique.indexOf(array[i])=== -1){
+        unique.push(array[i]);
+    }
+  }
+  return cb(unique);
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -124,6 +150,14 @@ uniq(names, function(uniqArr){
 
 //Code Here 
 
+function each(name, cb){
+  for(let i = 0 ; i < name.length ; i++){
+    cb(name[i], i)
+  }
+};
+
+
+
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,6 +174,11 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById (arr, idnum, cb){  
+  return cb(users.find(({id}) => id === '16t'))
+}
+
+
 
 // Do not edit the code below.
 var users = [
